ba1da5a076ec55bd507b8cda91864a46e0f9dc07
rankrev <307532500@qq.com>
2025-03-31 20:31:16 +0800

    exchange the c/C for command search and command history

---
+++ lua/plugins/extend-fzf.lua
@@ -0,0 +1,7 @@
+return {
+  "ibhagwan/fzf-lua",
+  keys = {
+    { "<leader>sc", "<cmd>FzfLua commands<cr>", desc = "Commands" },
+    { "<leader>sC", "<cmd>FzfLua command_history<cr>", desc = "Command History" },
+  },
+}
