901e45cb586cc298694fed042208b4a57700b7e9
gorun <307532500@qq.com>
2025-04-07 00:15:42 +0800

    vscode keymap
    deal with <leader>e in vscode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+if vim.g.vscode then
+  local vscode = require("vscode")
+  vim.keymap.set("n", "<leader>e", function()
+    vscode.action("workbench.files.action.focusFilesExplorer")
+  end)
+end
